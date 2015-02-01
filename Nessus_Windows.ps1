febc8e3164c3b62ea8e9d879e020a84a022432a7
Yuri Krysko <ykrysko@yahoo.com>
2015-02-01 17:01:39 -0500

    Corrected the way firewall rules are handled, added the function to check registry keys correctly

--- Nessus_Windows.ps1
+++ Nessus_Windows.ps1
@@ -11,11 +11,13 @@ $fwsrv =  Get-Service -Name "MpsSvc" | Where-Object {$_.status -eq "Stopped"}
 if ( $fwsrv -eq $null ) {
 Write-Host "Step #1. Modifying Windows Firewall rules" -ForegroundColor Red
 sleep 5
-netsh advfirewall firewall set rule name="File and Printer Sharing (SMB-In)" new enable=Yes
-netsh advfirewall firewall set rule name="File and Printer Sharing (NB-Session-In)" new enable=Yes
-netsh advfirewall firewall set rule name="Windows Management Instrumentation (ASync-In)" new enable=Yes
-netsh advfirewall firewall set rule name="Windows Management Instrumentation (DCOM-In)" new enable=Yes
-netsh advfirewall firewall set rule name="Windows Management Instrumentation (WMI-In)" new enable=Yes
+$fwvalue = "advfirewall set store gpo=$env:COMPUTERNAME`r`nadvfirewall firewall add rule name=`"File and Printer Sharing (SMB-In)`" dir=in protocol=TCP localport=445 action=allow`r`
+advfirewall firewall add rule name=`"File and Printer Sharing (NB-Session-In)`" dir=in protocol=TCP localport=139 action=allow`nadvfirewall firewall add rule name=`"Windows Management Instrumentation (ASync-In)`" dir=in protocol=TCP program=`"C:\Windows\system32\wbem\unsecapp.exe`" action=allow`r`
+advfirewall firewall add rule name=`"Windows Management Instrumentation (DCOM-In)`" dir=in protocol=TCP localport=135 program=`"C:\Windows\system32\svchost.exe`" action=allow`r`
+advfirewall firewall add rule name=`"Windows Management Instrumentation (WMI-In)`" dir=in protocol=TCP program=`"C:\Windows\system32\svchost.exe`" action=allow`n"
+Set-Content -Value $fwvalue -Path .\firewall.txt
+netsh -f .\firewall.txt
+Remove-Item -Path .\firewall.txt
 }
 else { 
 Write-Host "Step #1. Looks like Windows Firewall service is stopped. Skipping..." -ForegroundColor Red
@@ -44,18 +46,41 @@ if ( $userd -ne $null ) {
 Write-Host "Step #3. Enabling administrative shares on $env:COMPUTERNAME" -ForegroundColor Red
 sleep 5
 
-if ($(Test-Path -Path HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters\AutoShareServer) -eq $true) {
+function Test-Regedit {
+
+param (
+
+ [parameter(Mandatory=$true)]
+ [ValidateNotNullOrEmpty()]$Path,
+
+[parameter(Mandatory=$true)]
+ [ValidateNotNullOrEmpty()]$Value
+)
+
+try {
+Get-ItemProperty -Path $Path | Select-Object -ExpandProperty $Value -ErrorAction Stop | Out-Null
+ return $true
+ }
+
+catch {
+return $false
+
+	}
+}
+
+if ($(Test-Regedit -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters' -Value 'AutoShareServer') -eq $true) {
 	Remove-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters -Name "AutoShareServer"
 }
-elseif ($(Test-Path -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System\LocalAccountTokenFilterPolicy) -eq $true) {
+if ($(Test-Regedit -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters' -Value 'AutoShareWks') -eq $true) {
+	Remove-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters -Name "AutoShareWks"
+}
+if ($(Test-Regedit -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System' -Value 'LocalAccountTokenFilterPolicy') -eq $true) {
 	Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "LocalAccountTokenFilterPolicy" -Value 1 -Force
-	Write-Host "`nFinished messing with $env:COMPUTERNAME. Rebooting...`n" -ForegroundColor Green
-	sleep 5
-	#Restart-Computer -Force
 }
 else {
 	New-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "LocalAccountTokenFilterPolicy" -Value 1 -PropertyType "DWORD" -Force
-	Write-Host "`nFinished messing with $env:COMPUTERNAME. Rebooting...`n" -ForegroundColor Green
-	sleep 5
-	#Restart-Computer -Force
 }
+
+Write-Host "`nFinished messing with $env:COMPUTERNAME. Rebooting...`n" -ForegroundColor Green
+	sleep 5
+#Restart-Computer -Force
